20795f78ea0eeae741c10db6bdcfb36641f1dffd
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2017-03-21 22:45:14 +0500

    dont know what to write here

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import getModels from './models';
 export default async () => {
   const models = getModels(connect);
 
-  await Promise.all(Object.values(models).map(async model => model.sync({ force: true })))
+  await Promise.all(Object.values(models).map(model => model.sync({ force: true })))
     .then((values) => {
       models.Status.bulkCreate([
         {
